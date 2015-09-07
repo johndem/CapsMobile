46226731e43f08de28fa18da975fe90c6e0bf5fb
John Dematis <johndem13@gmail.com>
2015-09-07 13:54:39 +0300

    Dynamic Navigation Drawer Menu Items

--- app/src/main/java/csd/jt/capsmobile/BaseActivity.java
+++ app/src/main/java/csd/jt/capsmobile/BaseActivity.java
@@ -17,35 +17,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
 
     // Navigation Drawer variables
     private ActionBarDrawerToggle mDrawerToggle;
-    private String[] navArray = {"Home", "Volunteer", "Organization", "Event", "Search", "Login", "Sign up", "Sign out"};
-//    private DrawerLayout mDrawerLayout;
-//    private String mActivityTitle;
-//
-//    private ListView mDrawerList;
-//    private ArrayAdapter<String> mAdapter;
+    private String[] navArrayVisitor = {"Home", "Search", "Login", "Sign up"};
+    private String[] navArrayUser = {"Home", "My Profile", "Search", "Sign out"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_base);
-//
-//        // create navigation drawer
-//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        //mActivityTitle = getTitle().toString();
-//
-//        mDrawerList = (ListView) findViewById(R.id.navList);
-//
-//        //setupDrawer();
-//        addDrawerItems();
-
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setHomeButtonEnabled(true);
-
 
     }
 
@@ -96,48 +80,71 @@ public class BaseActivity extends AppCompatActivity {
         DrawerLayout mDrawerLayout = (DrawerLayout) fullLayout.findViewById(R.id.drawer_layout);
         ListView mDrawerList = (ListView) fullLayout.findViewById(R.id.navList);
 
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navArray));
 
-                mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //Toast.makeText(MainActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
-                if (position == 0) {
-                    Intent intent = new Intent(BaseActivity.this, MainActivity.class);
-                    startActivity(intent);
+        SharedPreferences myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
+        String id = myPrefs.getString("userId",null);
+
+        if (id == null) {
+            mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navArrayVisitor));
+
+            mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    if (position == 0) {
+                        Intent intent = new Intent(BaseActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                    if (position == 1) {
+                        Intent intent = new Intent(BaseActivity.this, SearchForm.class);
+                        startActivity(intent);
+                    }
+                    if (position == 2) {
+                        Intent intent = new Intent(BaseActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                    if (position == 3) {
+                        Intent intent = new Intent(BaseActivity.this, RegisterVolActivity.class);
+                        startActivity(intent);
+                    }
                 }
-                if (position == 1) {
-                    Intent intent = new Intent(BaseActivity.this, VolunteerActivity.class);
-                    startActivity(intent);
+            });
+        }
+        else {
+            mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navArrayUser));
+
+            mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    if (position == 0) {
+                        Intent intent = new Intent(BaseActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                    if (position == 1) {
+                        SharedPreferences innerPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
+                        String role = innerPrefs.getString("userRole",null);
+                        Intent intent;
+                        if (role.equals("vol"))
+                            intent = new Intent(BaseActivity.this, VolunteerActivity.class);
+                        else
+                            intent = new Intent(BaseActivity.this, OrganizationActivity.class);
+                        startActivity(intent);
+                    }
+                    if (position == 2) {
+                        Intent intent = new Intent(BaseActivity.this, SearchForm.class);
+                        startActivity(intent);
+                    }
+                    if (position == 3) {
+                        SharedPreferences myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = myPrefs.edit();
+                        editor.clear();
+                        editor.commit();
+                        Intent intent = new Intent(BaseActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
                 }
-                if (position == 2) {
-                    Intent intent = new Intent(BaseActivity.this, OrganizationActivity.class);
-                    startActivity(intent);
-                }
-                if (position == 3) {
-                    Intent intent = new Intent(BaseActivity.this, EventActivity.class);
-                    startActivity(intent);
-                }
-                if (position == 4) {
-                    Intent intent = new Intent(BaseActivity.this, SearchForm.class);
-                    startActivity(intent);
-                }
-                if (position == 5) {
-                    Intent intent = new Intent(BaseActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                }
-                if (position == 6) {
-                    Intent intent = new Intent(BaseActivity.this, RegisterVolActivity.class);
-                    startActivity(intent);
-                }
-                if (position == 7) {
-                    SharedPreferences myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = myPrefs.edit();
-                    editor.clear();
-                    editor.commit();
-                }
-            }
-        });
+            });
+        }
+
 
         mDrawerToggle = new ActionBarDrawerToggle(
                 this,
@@ -165,73 +172,6 @@ public class BaseActivity extends AppCompatActivity {
         getLayoutInflater().inflate(layoutResID, actContent, true);
         super.setContentView(fullLayout);
     }
-
-//    // build navigation drawer and add listener
-//    private void addDrawerItems() {
-////        String[] navArray = {"Home", "Volunteer", "Organization", "Event", "Search", "Login", "Sign up", "Sign out"};
-//        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navArray);
-//        mDrawerList.setAdapter(mAdapter);
-//
-//        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                //Toast.makeText(MainActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
-//                if (position == 1) {
-//                    Intent intent = new Intent(BaseActivity.this, VolunteerActivity.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 2) {
-//                    Intent intent = new Intent(BaseActivity.this, OrganizationActivity.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 3) {
-//                    Intent intent = new Intent(BaseActivity.this, EventActivity.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 4) {
-//                    Intent intent = new Intent(BaseActivity.this, SearchForm.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 5) {
-//                    Intent intent = new Intent(BaseActivity.this, LoginActivity.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 6) {
-//                    Intent intent = new Intent(BaseActivity.this, RegisterVolActivity.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 7) {
-//                    SharedPreferences myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
-//                    SharedPreferences.Editor editor = myPrefs.edit();
-//                    editor.clear();
-//                    editor.commit();
-//                }
-//            }
-//        });
-//    }
-
-//    private void setupDrawer() {
-//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-//                R.string.drawer_open, R.string.drawer_close) {
-//
-//            /** Called when a drawer has settled in a completely open state. */
-//            public void onDrawerOpened(View drawerView) {
-//                super.onDrawerOpened(drawerView);
-//                getSupportActionBar().setTitle("Navigation!");
-//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-//            }
-//
-//            /** Called when a drawer has settled in a completely closed state. */
-//            public void onDrawerClosed(View view) {
-//                super.onDrawerClosed(view);
-//                getSupportActionBar().setTitle(mActivityTitle);
-//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-//            }
-//        };
-//
-//        mDrawerToggle.setDrawerIndicatorEnabled(true);
-//        mDrawerLayout.setDrawerListener(mDrawerToggle);
-//    }
 
 
 }
--- app/src/main/java/csd/jt/capsmobile/MainActivity.java
+++ app/src/main/java/csd/jt/capsmobile/MainActivity.java
@@ -19,46 +19,10 @@ import android.widget.ListView;
 
 public class MainActivity extends BaseActivity {
 
-
-    // Navigation Drawer variables
-//    private ActionBarDrawerToggle mDrawerToggle;
-//    private DrawerLayout mDrawerLayout;
-//    private String mActivityTitle;
-//
-//    private ListView mDrawerList;
-//    private ArrayAdapter<String> mAdapter;
-
-
-//    SharedPreferences myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
-//    String id = myPrefs.getString("userId",null);
-//
-//    if (id == null)
-//            Toast.makeText(SearchForm.this, "Not logged in", Toast.LENGTH_SHORT).show();
-//    else
-//            Toast.makeText(SearchForm.this, "Logged in!", Toast.LENGTH_SHORT).show();
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-        // create navigation drawer
-//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        mActivityTitle = getTitle().toString();
-//
-//        mDrawerList = (ListView) findViewById(R.id.navList);
-//        addDrawerItems();
-//
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setHomeButtonEnabled(true);
-//
-//
-//        setupDrawer();
-
-
 
         //Set Categories Listeners
         ImageButton cat1 = (ImageButton) findViewById(R.id.cat1);
@@ -125,83 +89,15 @@ public class MainActivity extends BaseActivity {
 
     }
 
-    // build navigation drawer and add listener
-//    private void addDrawerItems() {
-//        String[] navArray = {"Home", "Volunteer", "Organization", "Event", "Search", "Login", "Sign up", "Sign out"};
-//        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navArray);
-//        mDrawerList.setAdapter(mAdapter);
-//
-//        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                //Toast.makeText(MainActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
-//                if (position == 1) {
-//                    Intent intent = new Intent(MainActivity.this, VolunteerActivity.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 2) {
-//                    Intent intent = new Intent(MainActivity.this, OrganizationActivity.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 3) {
-//                    Intent intent = new Intent(MainActivity.this, EventActivity.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 4) {
-//                    Intent intent = new Intent(MainActivity.this, SearchForm.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 5) {
-//                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 6) {
-//                    Intent intent = new Intent(MainActivity.this, RegisterVolActivity.class);
-//                    startActivity(intent);
-//                }
-//                if (position == 7) {
-//                    SharedPreferences myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
-//                    SharedPreferences.Editor editor = myPrefs.edit();
-//                    editor.clear();
-//                    editor.commit();
-//                }
-//            }
-//        });
-//    }
-//
-//    private void setupDrawer() {
-//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-//                R.string.drawer_open, R.string.drawer_close) {
-//
-//            /** Called when a drawer has settled in a completely open state. */
-//            public void onDrawerOpened(View drawerView) {
-//                super.onDrawerOpened(drawerView);
-//                getSupportActionBar().setTitle("Navigation!");
-//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-//            }
-//
-//            /** Called when a drawer has settled in a completely closed state. */
-//            public void onDrawerClosed(View view) {
-//                super.onDrawerClosed(view);
-//                getSupportActionBar().setTitle(mActivityTitle);
-//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-//            }
-//        };
-//
-//        mDrawerToggle.setDrawerIndicatorEnabled(true);
-//        mDrawerLayout.setDrawerListener(mDrawerToggle);
-//    }
-//
+
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-//        mDrawerToggle.syncState();
     }
-//
+
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-//        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -215,10 +111,6 @@ public class MainActivity extends BaseActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
-//        if (mDrawerToggle.onOptionsItemSelected(item)) {
-//            return true;
-//        }
 
         return super.onOptionsItemSelected(item);
     }
